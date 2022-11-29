b7028abe1b99dbc769e64ae5a7c9a698af4b0474
pagioro <pagioro@hotmail.com>
2022-11-29 16:00:47 +0000

    trying to fix 404 error

--- boutique_ado/urls.py
+++ boutique_ado/urls.py
@@ -1,3 +1,5 @@
+from .views import handler404
+
 """boutique_ado URL Configuration
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
@@ -12,7 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from .views import handler404
+
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
